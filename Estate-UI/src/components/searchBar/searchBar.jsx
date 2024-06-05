10f6041a0653442c1aaf0812813d48c94bc8823c
swasthik-007 <mohanty.swastik4@gmail.com>
2024-06-06 02:15:05 +0530

    final completion of Home Page

--- Estate-UI/src/components/searchBar/searchBar.jsx
+++ Estate-UI/src/components/searchBar/searchBar.jsx
@@ -1,12 +1,55 @@
-import './searchBar.scss' ;
+import { useState } from "react";
+import "./searchBar.scss";
 
-function SearchBar(){
+const types = ["buy", "rent"];
 
-return(
-    <div className='searchBar'>
-        SearchBar
+function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
     </div>
-);
+  );
 }
 
 export default SearchBar;
