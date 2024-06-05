3105ddc014c1b4ba6811a6a2254f67241168bc6a
swasthik-007 <mohanty.swastik4@gmail.com>
2024-06-06 01:21:49 +0530

    took care of the navbar component

---
+++ Estate-UI/src/App.jsx
@@ -0,0 +1,14 @@
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+
+
+  return (
+    <div className="layout">
+    <Navbar/>
+    </div>
+  )
+}
+
+export default App;
